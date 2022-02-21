d0d547cf7a326800b8b79ac83da44f994fb57fc8
Akash <kadamakash131724@gmail.com>
2022-02-21 17:34:59 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
-import './App.css';
-
+ 
+import { useState, useEffect } from 'react';
+import { Link, Routes, Route } from "react-router-dom";
+import Page1 from './components/pages/Page1';
+import Page2 from './components/pages/Page2';
+import Page3 from './components/pages/Page3';
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        <Routes>
+          <Route path="/" element={<Page1 />} />
+          <Route path="page2" element={<Page2 />} />
+          <Route path="page3" element={<Page3 />} />
+        </Routes>
+      }
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/pages/Page2.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import { BiLike } from 'react-icons/bi'
+import { FiShare } from 'react-icons/fi'
+import { AiFillEye } from 'react-icons/ai'
+import { useState, useEffect } from 'react'
+import { Link, Routes, Route } from "react-router-dom";
+const Page2 = () => {
+    const url = 'http://www.mocky.io/v2/59ac28a9100000ce0bf9c236' ;
+    const [pages2, setPages2] = useState({})
+   const fetchPages = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json()
+        setPages2(data)
+        console.log(data.posts[0])
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+     const {page,posts} = pages2;
+     useEffect(() =>{
+      fetchPages()
+     },[])
+  // const [data, setData] = useState([]);
+  // console.log(posts)
+  // const [value, setValue] = useState('date');
+  // console.log(data);
+
+
+  // useEffect(() => {
+  //   const sortArray = (type) => {
+  //     const types = {
+  //       'date': 'event_date',
+  //       'likes': 'likes',
+  //       'views': 'views',
+  //       'shares': 'shares'
+  //     };
+  //     const sortpages = types[type];
+
+  //     const sorted =posts.sort((a, b) => a[sortpages] < b[sortpages] ? 1 : -1)
+  //     setData([...sorted]);
+  //     console.log(sorted)
+  //   };
+  //   sortArray(value);
+  // }, [value]);
+
+ 
+    return (
+      <>
+        <div className='page'>
+          pages<Link to="/"><button>1</button></Link> 
+          <Link to="/page2"><button>2</button></Link> 
+          <Link to="/page3"><button>3</button></Link> 
+        </div>
+        <label>Sort by increasing order</label>
+        {/* <select value={value} onChange={e => setValue(e.target.value)}>  */}
+        <select > 
+          <option value="date">date</option>
+          <option value="likes">likes</option>
+          <option value="shares">shares</option>
+          <option value="views">views</option>
+        </select>
+  
+        {posts?.map((item) => {
+          const { id, event_date, event_name, likes, shares, thumbnail_image, views } = item;
+          let date = new Date(event_date * 1000);
+          //  console.log(date);
+          return (<div key={event_date} className='post'>
+            <div className='horizontal_center'>
+              <header>
+                <p>{event_name.length < 25 ? event_name : event_name.slice(0, -25)}</p>
+                <h4>{date.toDateString()}</h4>
+              </header>
+              <img className='img_result' src={thumbnail_image} alt={event_name} />
+              <footer>
+                <div className='center_footer'>
+                  <p><button className='btn'><BiLike className='icon' /></button><span>{likes}</span></p>
+                  <p><button className='btn'><AiFillEye className='icon' /></button><span>{views}</span></p>
+                  <p><button className='btn'><FiShare className='icon' /></button><span>{shares}</span></p>
+                </div>
+              </footer>
+              <div className='divider'></div>
+            </div>
+          </div>)
+        })
+        }
+             <div className='page'>
+          pages
+          <Link to="/"><button>1</button></Link> 
+          <Link to="/page2"><button>2</button></Link> 
+          <Link to="/page3"><button>3</button></Link>       
+        </div>
+      </>
+    )
+ 
+  
+}
+
+export default Page2
